944b31d91344b2912fe69769a48ca9476d0af713
Jishan <129368844+imjishan@users.noreply.github.com>
2024-09-17 22:05:55 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default async function RootLayout({
       className={`bg-primary`}
     >
       <body>
-            <div className="z-20 relative">
+            <div className="z-20 relative rounded-3xl">
               <Header />
             </div>
 {children}
