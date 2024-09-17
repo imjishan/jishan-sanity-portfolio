0c8a82634f5043d77f33946abcadb59d0eb9513a
Jishan <129368844+imjishan@users.noreply.github.com>
2024-09-17 21:47:39 +0530

    created navigation and social links

---
+++ components/global/Navdata.js
@@ -0,0 +1,13 @@
+export const Links = [
+  { title: "Home", href: "/" },
+  { title: "Projects", href: "/projects" },
+  { title: "About", href: "/about" },
+  { title: "Expertise", href: "/expertise" },
+  { title: "Contact", href: "/contact" },
+];
+export const Footerlinks = [
+  { title: "Github", href: "https://github.com/imjishan" },
+  { title: "LinkedIn", href: "https://linkedin.com/in/jishanahmed11" },
+  { title: "Twitter", href: "https://x.com/JishanCodes" },
+  { title: "Instagram", href: "https://instagram.com/jishan.dv" },
+];
